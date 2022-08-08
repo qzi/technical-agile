67654278789eff34493601649cc5c71c0413c4fa
Leon Qiu <i@leonvision.online>
2022-08-08 16:09:54 +0800

    add chai

--- cucumberjs-test/test/step_definitions/login_stepdefs.js
+++ cucumberjs-test/test/step_definitions/login_stepdefs.js
@@ -1,38 +1,39 @@
 import assert from "assert";
 import { Given, When, Then } from "@cucumber/cucumber";
+import { expect }  from "chai";
 
 
-  Given('I have previously created a username: {string}', function (username) {
-    this.username = username;
-  });
+Given("I have previously created a username: {string}", function (username) {
+  this.username = username;
+});
 
-  Given ('I have previously created a password: {string}', function (password) {
-    this.password = password;
-  });
+Given("I have previously created a password: {string}", function (password) {
+  this.password = password;
+});
 
-  When('I enter my username correctly', function () {
-    assert.equal("username",this.username);
-  });
+When("I enter my username correctly", function () {
+  assert.equal("username", this.username);
+});
 
-  When('I enter my passwrod correctly', function () {
-    assert.equal("password", this.password);
-  });
+When("I enter my passwrod correctly", function () {
+  assert.equal("password", this.password);
+});
+
+When("I click on the button {string}", function (button) {
+  if (button == "login") {
+    this.isSubmit = true;
+  } else {
+    this.isSubmit = false;
+  }
+});
 
 
-  When('I click on the button {string}', function (button) {
-    if(button == "login") {
-      this.isSubmit = true;
-    } else {
-      this.isSubmit = false;
-    }
-  });
+Then("I got a feedback {string}", function (expected) {
 
-
-  Then('I got a feedback {string}', function (feedback) {
-    if (this.isSubmit) {
-      assert.equal("login successfully", feedback);
-    }else {
-      console.log("login failed")
-    }
-   
-  });
+  if (this.isSubmit) {
+    // assert.equal("login successfully", expected);
+    expect("login successfully").to.eql(expected);
+  } else {
+    console.log("login failed");
+  }
+});
